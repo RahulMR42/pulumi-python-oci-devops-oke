cde1979a9694cdcd0af2c49344ecada8b14ac1ec
Rahul M R <test@example.com>
2022-06-06 15:25:15 +0530

    V0.12

--- resources/devops.py
+++ resources/devops.py
@@ -7,17 +7,18 @@ from urllib.parse import quote
 from resources.random import random_string
 
 
+
 class devops:
     def __init__(self):
         self.config = Config()
-        self.random_string = random_string()
+        self.random_string = random_string().fetch_random_string()
 
     def create_devops_project(self,notification_topic):
 
         try:
             devops_project = oci.devops.Project("devops_project",
                                                 compartment_id=self.config.get('compartment_ocid'),
-                                                name=self.config.get('oci_devops_project_name'),
+                                                name=f"{self.config.get('oci_devops_project_name')}_{self.random_string}",
                                                 notification_config=oci.devops.ProjectNotificationConfigArgs(
                                                     topic_id=notification_topic.id,
                                                 ),)
@@ -49,10 +50,9 @@ class devops:
                 Repo.clone_from(oci_remote_url,f"{self.config.get('devops_coderepo_name')}_local")
             shutil.copytree(self.config.get('github_clone_path'), f"{self.config.get('devops_coderepo_name')}_local",dirs_exist_ok=True ,ignore=shutil.ignore_patterns('.git'))
             repo = Repo(f"{self.config.get('devops_coderepo_name')}_local")
-            remote_name = self.random_string
-            remote = repo.create_remote(remote_name, url=oci_remote_url)
+            remote = repo.remote(name='origin')
             repo.git.add(all=True)
-            repo.index.commit(f"Pushing via remote - {remote_name}")
+            repo.index.commit(f"Pushing via remote - Origin ")
             remote.push(refspec='{}:{}'.format('main', 'main'))
         except Exception as error:
             print("Clone and Push code failed " + str(error))
@@ -70,9 +70,197 @@ class devops:
         except Exception as error:
             print("Deploy env creation is failing " + str(error))
 
+    def create_container_devops_artifact(self,config,devops_project):
+        try:
+            namespace = oci.objectstorage.get_namespace(compartment_id=config.get('compartment_ocid'))
+            oci_image_uri=f"{os.environ['TF_VAR_region']}.ocir.io/{namespace.namespace}/{config.get('app_name_prefix')}_{config.get('oci_containerrepo_displayname')}:" + "${BUILDRUN_HASH}"
+            artifact_containerrepo = oci.devops.DeployArtifact("artifact_containerrepo",
+                                                                argument_substitution_mode=config.require('containerartifact_argumentsubstitutionmode'),
+                                                                deploy_artifact_source=oci.devops.DeployArtifactDeployArtifactSourceArgs(
+                                                                    deploy_artifact_source_type=config.require('containerartifact_sourcetype'),
+                                                                    image_uri=oci_image_uri,
+                                                                ),
+                                                                deploy_artifact_type=config.require('containerartifact_artifacttype'),
+                                                                display_name=config.require('containerartifact_displayname'),
+                                                                project_id=devops_project.id)
+            return  artifact_containerrepo,oci_image_uri
 
 
+        except Exception as error:
+            print("Devops  container erpo artifact creation failed" + str(error))
+
+    def create_kubernetes_devops_artifact(self,config,devops_project,oci_image_uri):
+        try:
+            with open('manifest/kubernetes-deploymentspec.yaml','r') as stream:
+                filedata = stream.read()
+            filedata=filedata.replace('oci_image_url',oci_image_uri)
+
+            artifact_kubernetes = oci.devops.DeployArtifact("artifact_kubernetes",
+                                                             argument_substitution_mode="SUBSTITUTE_PLACEHOLDERS",
+                                                             deploy_artifact_source=oci.devops.DeployArtifactDeployArtifactSourceArgs(
+                                                                 base64encoded_content=filedata,
+                                                                 deploy_artifact_source_type=config.require('deploy_artifact_source_type'),
+                                                             ),
+                                                             deploy_artifact_type=config.require('kuberartifact_sourcetype'),
+                                                             display_name=config.require('kuberartifact_displayname'),
+                                                             project_id=devops_project.id)
+
+            return artifact_kubernetes
 
 
+        except Exception as error:
+            print("Kubernetes artifact creation failed " + str(error))
 
 
+    def create_build_pipeline(self,config,devops_project):
+        try:
+            devops_build_pipeline = oci.devops.BuildPipeline("devops_build_pipeline",
+                                                              build_pipeline_parameters=oci.devops.BuildPipelineBuildPipelineParametersArgs(
+                                                                  items=[oci.devops.BuildPipelineBuildPipelineParametersItemArgs(
+                                                                      name="BUILDRUN_HASH",
+                                                                      default_value="NA",
+                                                                      description="Tag for container image"
+                                                                  )]
+                                                              ),
+                                                              description=config.require('devop_buildpipeline_description'),
+                                                              display_name=config.require('devop_buildpipeline_name'),
+                                                              project_id=devops_project.id)
+            return  devops_build_pipeline
+        except Exception as error:
+            print ("Buildpipeline creation failed" + str(error))
+
+    def create_deploy_pipeline(self,config,devops_project):
+        try:
+            ocidevops_pipeline = oci.devops.DeployPipeline("ocidevops_pipeline",
+                                                            deploy_pipeline_parameters=oci.devops.DeployPipelineDeployPipelineParametersArgs(
+                                                                items=[oci.devops.DeployPipelineDeployPipelineParametersItemArgs(
+                                                                    default_value=config.require('deploypipeline_param_defaultvalue'),
+                                                                    description=config.require('deploypipeline_param_description'),
+                                                                    name=config.require('deploypipeline_param_name'),
+                                                                )],
+                                                            ),
+                                                            description=config.require('deploypipeline_description'),
+                                                            display_name=config.require('deploypipeline_displayname'),
+                                                            project_id=devops_project.id)
+            return  ocidevops_pipeline
+
+        except Exception as error:
+            print ("Deploypipeline creation failed" + str(error))
+
+    def create_buildstage(self,config,devops_coderepo,devops_build_pipeline):
+        try:
+            build_stage_appbuild = oci.devops.BuildPipelineStage("build_stage_appbuild",
+                                                                  build_pipeline_id=devops_build_pipeline.id,
+                                                                  build_pipeline_stage_predecessor_collection=oci.devops.BuildPipelineStageBuildPipelineStagePredecessorCollectionArgs(
+                                                                      items=[oci.devops.BuildPipelineStageBuildPipelineStagePredecessorCollectionItemArgs(
+                                                                          id=devops_build_pipeline.id,
+                                                                      )],
+                                                                  ),
+                                                                  build_pipeline_stage_type=config.require('buildstage_stagetype'),
+                                                                  build_source_collection=oci.devops.BuildPipelineStageBuildSourceCollectionArgs(
+                                                                      items=[oci.devops.BuildPipelineStageBuildSourceCollectionItemArgs(
+                                                                          branch=config.require('buildstage_branchname'),
+                                                                          connection_type=config.require('buildstage_connection_type'),
+                                                                          name=config.require('buildstage_sourcename'),
+                                                                          repository_id=devops_coderepo.id,
+                                                                     )],
+                                                                  ),
+                                                                  build_spec_file=config.require('build_spec_file'),
+                                                                  description=config.require('buildstage_description'),
+                                                                  display_name=config.require('buildstage_displayname'),
+                                                                  image=config.require('buildstage_image'),
+                                                                  primary_build_source=config.require('buildstage_sourcename'),
+                                                                  )
+            return  build_stage_appbuild
+        except Exception as error:
+            print("Build stage creation failed" + str(error))
+
+    def create_uploadartifact_stage(self,config,devops_build_pipeline,build_stage_appbuild,artifact_containerrepo):
+        try:
+            uploadartifact_stage = oci.devops.BuildPipelineStage("uploadartifact_stage",
+                                                                  build_pipeline_id=devops_build_pipeline.id,
+                                                                  build_pipeline_stage_predecessor_collection=oci.devops.BuildPipelineStageBuildPipelineStagePredecessorCollectionArgs(
+                                                                      items=[oci.devops.BuildPipelineStageBuildPipelineStagePredecessorCollectionItemArgs(
+                                                                          id=build_stage_appbuild.id,
+                                                                      )],
+                                                                  ),
+                                                                  build_pipeline_stage_type=config.require('uploadartifact_stagetype'),
+                                                                  deliver_artifact_collection=oci.devops.BuildPipelineStageDeliverArtifactCollectionArgs(
+                                                                      items=[oci.devops.BuildPipelineStageDeliverArtifactCollectionItemArgs(
+                                                                          artifact_id=artifact_containerrepo.id,
+                                                                          artifact_name=config.require('uploadartifact_source'),
+                                                                      )],
+                                                                  ),
+                                                                  description=config.require('uploadartifact_description'),
+                                                                  display_name=config.require('uploadartifact_displayname'))
+            return  uploadartifact_stage
+
+
+        except Exception as error:
+            print("Uploadartifact stage creation failed" + str(error))
+
+    def create_triggerdeploy_stage(self,config,uploadartifact_stage,devops_build_pipeline,devops_deploy_pipeline):
+        try:
+            trigerdeployment_stage = oci.devops.BuildPipelineStage("trigerdeployment_stage",
+                                                                    build_pipeline_id=devops_build_pipeline.id,
+                                                                    build_pipeline_stage_predecessor_collection=oci.devops.BuildPipelineStageBuildPipelineStagePredecessorCollectionArgs(
+                                                                        items=[oci.devops.BuildPipelineStageBuildPipelineStagePredecessorCollectionItemArgs(
+                                                                            id=uploadartifact_stage.id,
+                                                                        )],
+                                                                    ),
+                                                                    build_pipeline_stage_type=config.require('trigerdeployment_stagetype'),
+                                                                    deploy_pipeline_id=devops_deploy_pipeline.id,
+                                                                    description=config.require('trigerdeployment_description'),
+                                                                    display_name=config.require('trigerdeployment_name'),
+                                                                    is_pass_all_parameters_enabled=config.require('trigerdeployment_parseparamenabled'))
+            return  trigerdeployment_stage
+
+        except Exception as error:
+            print("Trigerdeploy stage creation failed" + str(error))
+
+    def create_deployment_stage(self,config,devops_deploy_pipeline,artifact_kubernetes,deploy_oke_env):
+        try:
+            deployto_oke = oci.devops.DeployStage("deployto_oke",
+                                                   deploy_pipeline_id=devops_deploy_pipeline.id,
+                                                   deploy_stage_predecessor_collection=oci.devops.DeployStageDeployStagePredecessorCollectionArgs(
+                                                       items=[oci.devops.DeployStageDeployStagePredecessorCollectionItemArgs(
+                                                           id=devops_deploy_pipeline.id,
+                                                       )],
+                                                   ),
+                                                   deploy_stage_type=config.require('deploystage_oke_type'),
+                                                   description=config.require('deploystage_oke_description'),
+                                                   display_name=config.require('deploystage_oke_name'),
+                                                   kubernetes_manifest_deploy_artifact_ids=[artifact_kubernetes.id],
+                                                   oke_cluster_deploy_environment_id=deploy_oke_env.id,
+                                                   rollback_policy=oci.devops.DeployStageRollbackPolicyArgs(
+                                                       policy_type=config.require('deploystage_oke_rollbackpolicy'),
+                                                   ))
+            return deployto_oke
+
+        except Exception as error:
+            print("Deployment stage creation failed" + str(error))
+
+    def create_buildrun(self,config,devops_build_pipeline):
+        try:
+            if (config.require('autobuild_run') == True):
+                print("Starting build run ...")
+                auto_buildrun = oci.devops.BuildRun("auto_buildrun",
+                                                     build_pipeline_id=devops_build_pipeline.id,
+                                                     build_run_arguments=oci.devops.BuildRunBuildRunArgumentsArgs(
+                                                         items=[oci.devops.BuildRunBuildRunArgumentsItemArgs(
+                                                             name="BUILDRUN_HASH",
+                                                             value="NA",
+                                                         )],
+                                                     ),
+                                                     commit_info=oci.devops.BuildRunCommitInfoArgs(
+                                                         commit_hash="",
+                                                         repository_branch="",
+                                                         repository_url="",
+                                                     ),
+                                                     display_name=f"buildrun-ocidevops-with-pulumi-{self.random_string}")
+                return  auto_buildrun
+
+            else:
+                print("Auto build run is not enabled,enabled it - pulumi config set autobuild_run True  and pulumi up. ")
+        except Exception as error:
+            print("Auto buildrun failed" + str(error))
--- resources/policies.py
+++ resources/policies.py
@@ -9,24 +9,24 @@ class policies:
                                                         compartment_id=os.environ['TF_VAR_tenancy_ocid'],
                                                         description=config.get('dg_description'),
                                                         matching_rule=f"Any {{ALL {{resource.type = 'devopsdeploypipeline', resource.compartment.id = '{config.get('compartment_ocid')}'}},ALL {{resource.type = 'devopsrepository', resource.compartment.id = '{config.get('compartment_ocid')}'}},ALL {{resource.type = 'devopsbuildpipeline', resource.compartment.id = '{config.get('compartment_ocid')}'}}}}",
-                                                        name=f"{config.get('dg_name')}_{random_string_value.result}",
+                                                        name=f"{config.get('dg_name')}_{random_string_value}",
                                                         )
             return pulumi_devopsdg
 
         except Exception as error:
             print("DG Creation failed " + str(error))
 
-    def create_policies(self,config,pulumi_devopsdg):
+    def create_policies(self,config,random_string_value):
         try:
             pulumi_devops_policies = oci.identity.Policy("pulumi_devops_policies",
                                                          compartment_id=config.get('compartment_ocid'),
                                                          description=config.get('policies_description'),
-                                                         name=f"{config.get('policies_name')}_{random_string()}",
-                                                         statements=[
-                                                             f"'Allow group Administrators to manage devops-family in compartment id {config.get('compartment_ocid')}'",
-                                                             f"Allow group Administrators to manage all-artifacts in compartment id {config.get('compartment_ocid')}'",
-                                                             f"Allow dynamic-group {pulumi_devopsdg.name} to manage all-resources in compartment id {config.get('compartment_ocid')}'",
-                                                         ],
+                                                         name=f"{config.get('policies_name')}_{random_string_value}",
+                                                          statements=[
+                                                             f"Allow group Administrators to manage devops-family in compartment id {config.get('compartment_ocid')}",
+                                                             f"Allow group Administrators to manage all-artifacts in compartment id {config.get('compartment_ocid')}",
+                                                             f"Allow dynamic-group {config.get('dg_name')}_{random_string_value} to manage all-resources in compartment id {config.get('compartment_ocid')}",
+                                                          ],
                                                          )
             return pulumi_devops_policies
 
